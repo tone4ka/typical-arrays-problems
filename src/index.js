c71601bb5ea7118b587f49875f19f0c0ba94d658
tone4ka <toniamik@gmail.com>
2020-10-03 20:56:22 +0300

    feat: add basic function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(Array.isArray(array)&&array.length>0){
+    return array.reduce((minItem, current)=>Math.min(minItem,current));
+  }else return 0
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array)&&array.length>0){
+    return array.reduce((maxItem, current)=>Math.max(maxItem,current));
+  }else{
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array)&&array.length>0){
+    return array.reduce((sum, current)=>sum+current)/array.length;
+  }else{
+    return 0;
+  }
 }
